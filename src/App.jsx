e0382690d36836edd40c689b738a3d054958dab4
HariHaran212 <hariharan2004121@gmail.com>
2025-02-21 11:16:10 +0530

    V-1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css'
 
 import { useState, useEffect } from 'react'
